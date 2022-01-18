d9e6d0990c7f023f2987c72c73ce352e7177c2f5
Daniela Serafim Soares <danyelaserafim@gmail.com>
2022-01-18 14:32:58 -0300

    Update index

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header () {
     return (
         <S.Nav>
             <Link to='/'> Home </Link>
-            <Link to='/materias'> Matéris </Link>
+            <Link to='/materias'> Matérias </Link>
             <Link to='/alunos'> Alunos </Link>
         </S.Nav>
     )
